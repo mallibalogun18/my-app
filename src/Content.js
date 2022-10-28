39bb8d1885f86ec76f8f26fcea7727ee0d9f2264
Mohammed Allibalogun <mallibalogun18@gmail.com>
2022-10-28 13:32:53 -0400

    React styles/Click Events

--- src/Content.js
+++ src/Content.js
@@ -5,11 +5,27 @@ const Content = () => {
         return names[int]
       }
     
+    const handleClick = () =>{
+        console.log('You clicked it')
+    }
+    
+    const handleClick2= (name) =>{
+        console.log(`${name} was clicked`)
+    }
+
+    const handleClick3= (e) =>{
+        console.log(e.target)
+    }
+
+
     return (
        <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}
             </p>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={() => {handleClick2('Dave')}}>Click it</button>
+            <button onClick={(e) => {handleClick3(e)}}>Click it</button>
        </main>
     )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,10 @@
 const Header = () => {
+    // const headerStyle = {
+    //     backgroundColor: "royalblue",
+    //      color: '#fff'
+    // }; 
+    //When using inStyle you will insert the variable headerStyle in the style={}
+
     return (
         <header>
             <h1>Grocieris List</h1>
